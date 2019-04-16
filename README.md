3d9aed9dc7d9c0db942e534e8ce9c941888fe5f0
Nelu Soimaru <nsoimaru@gmail.com>
2019-04-16 21:16:12 +0300

    laraStart first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<h1>Starter template</h1>
+
+Based on Laravel 5.8 Framework and Admin LTE 3
+
+Version 1.0.0.0 16.04.2019
+Feature:
+1. Laravel 5.8
+2. Admin LTE 3
+3. Bootstrap 4.3.1
+4. Font Awesome
+
+Installations:
+1. composer install
+2. npm install
+3. copy .env.example to .env and change DB credentials to yours credentials
+5. php artisan make:auth
+6. php artisan:migrate
+
+Enjoy
+
+Resources:
+https://www.youtube.com/channel/UCQI-Ym2rLZx52vEoqlPQMdg
+https://www.youtube.com/channel/UCnj1BK9TU32-bOlZ9415fuw
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Lara Start</title>
+  <link rel="stylesheet" href="/css/app.css">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+
+  <!-- Navbar -->
+    @include("layouts/_partials/navTop")
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+    @include("layouts/_partials/navLeft")
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Lara Start</h1>
+
+          </div><!-- /.col -->
+          @include("layouts/_partials/breadcrumb")
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          {{-- content goes here!!!! --}}
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+    <div class="p-3">
+      <h5>Title</h5>
+      <p>Sidebar content</p>
+    </div>
+  </aside>
+  <!-- /.control-sidebar -->
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      ***** APP
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2014-2019 <a href="https://nartex.ro">nartex</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+<script src="/js/app.js"></script>
+</body>
+</html>
